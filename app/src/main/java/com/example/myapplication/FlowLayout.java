fd5a82289634b265c5c6257b22cb6226ea29310f
zhouzheng <zhouzheng@zhouzhengs-MacBook-Pro.local>
2021-04-08 00:11:50 -0700

    view

---
+++ app/src/main/java/com/example/myapplication/FlowLayout.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.ViewGroup;
+
+public final class FlowLayout extends ViewGroup {
+    public FlowLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
